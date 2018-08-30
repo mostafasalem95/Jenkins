f0e159a7bf823a59c2c367898fd1a9d17372758c
Mostafa Salem <mostafasalem95@gmail.com>
2018-08-30 12:55:23 +0200

    first commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,12 +3,12 @@ insert into customer(id, name, address) values(2, 'Johans Bulk AB', 'Balkvägen 
 insert into customer(id, name, address) values(3, 'Haralds Värdetransporter AB', 'Budgetvägen 1, 333 33 Uppsala');
 
 
-insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005399401', 'ABC123', 'Processing',  1);
-insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009093588', 'DEF456', 'Processing', 1);
-insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009048066', 'GHI789', 'Processing', 1);
+insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005399401', 'ABC123', 'Test',  1);
+insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009093588', 'DEF456', 'Test', 1);
+insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009048066', 'GHI789', 'Test', 1);
 
-insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005388011', 'JKL012', 'Processing', 2);
-insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005387949', 'MNO345', 'Processing', 2);
+insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005388011', 'JKL012', 'Test', 2);
+insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005387949', 'MNO345', 'Test', 2);
 
-insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009047066', 'PQR678', 'Processing', 3);
-insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005387055', 'STU901', 'Processing', 3);
+insert into vehicle(id, reg_nr, status, customer_id) values('VLUR4X20009047066', 'PQR678', 'Test', 3);
+insert into vehicle(id, reg_nr, status, customer_id) values('YS2R4X20005387055', 'STU901', 'Test', 3);
